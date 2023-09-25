8091bdbcd7dc978081687ae1e3ab48556795e0cb
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2023-09-25 20:18:02 -0300

    Add files via upload

---
+++ Models/DonationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WoofHub_App.Models.Enums;
+
+namespace WoofHub_App.Models
+{
+    public class DonationModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public float? Value { get; set; }
+        [Required]
+        public CCDonationEnum CCDonation { get; set; }
+        public string? Description { get; set; }
+    }
+}
