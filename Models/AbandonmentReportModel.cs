574474e31b8bfa7231eea12a01f1ee4fc19119b9
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2023-09-25 20:55:09 -0300

    Add files via upload

---
+++ Models/AbandonmentReportModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WoofHub_App.Models.Enums;
+
+namespace WoofHub_App.Models
+{
+    public class AbandonmentReport
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateOnly? Date { get; set; }
+        [Required]
+        public required AdressModel AbandonmentAdress { get; set; }
+        public string? Description { get; set; }
+    }
+}
