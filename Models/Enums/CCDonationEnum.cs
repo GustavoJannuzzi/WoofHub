4c889a76ddd1179923410db69dca13b1a227d039
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2023-09-25 20:17:07 -0300

    Added Centro de Custo Enum

---
+++ Models/Enums/CCDonationEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace WoofHub_App.Models.Enums
+{
+    public enum CCDonationEnum
+    {
+        [Description("CustosPet")]
+        CustosPet = 1,
+
+        [Description("CustosAdm")]
+        CustosAdm = 2
+    }
+}
